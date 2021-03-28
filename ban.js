1066be36c112c5952fad8e72e31f8d56d80bfe65
Limnuz <53657036+Limnuz@users.noreply.github.com>
2021-03-28 17:08:32 -0300

    primeira versao

---
+++ ban.js
@@ -0,0 +1,39 @@
+var topo = document.querySelector("div#top")
+var esquerda = document.querySelector("div#left")
+var direita = document.querySelector("div#right")
+
+var folders = [['seduzir','https://www.escoladeseducao.com/matricule-se/?ref=D50695930P'], ['orgasmo','https://go.hotmart.com/O50734083K?dp=1']]
+
+var folderTop = randomFolder()
+try {
+topo.innerHTML= `<a href=${folders[folderTop][1]}>  <img src="${folders[folderTop][0]}/728x90.jpg"> </a>`
+} catch (e){
+    console.log(e)
+}
+
+var folderLeft = randomFolder()
+try {
+esquerda.innerHTML= `<a href=${folders[folderLeft][1]}>  <img src="${folders[folderLeft][0]}/300x250.jpg"> </a>`
+} catch (e){
+    console.log(e)
+}
+
+var folderRight = randomFolder()
+try {
+direita.innerHTML= `<a href=${folders[folderRight][1]}>  <img src="${folders[folderRight][0]}/300x250.jpg"> </a>`
+} catch (e){
+    console.log(e)
+}
+
+
+function randomFolder (qtd = folders.length){
+    var result = Math.random() * qtd
+    return Math.floor(result)
+}
+
+
+
+
+
+
+
