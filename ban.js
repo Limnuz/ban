603043c24a66feb17c8db27ddff131c9bf9d0d66
Limnuz <53657036+Limnuz@users.noreply.github.com>
2021-03-28 18:12:07 -0300

    adicionado curso de informatica

--- ban.js
+++ ban.js
@@ -2,7 +2,7 @@ var topo = document.querySelector("div#top")
 var esquerda = document.querySelector("div#left")
 var direita = document.querySelector("div#right")
 
-var folders = [['seduzir','https://www.escoladeseducao.com/matricule-se/?ref=D50695930P'], ['orgasmo','https://go.hotmart.com/O50734083K?dp=1']]
+var folders = [['seduzir','https://www.escoladeseducao.com/matricule-se/?ref=D50695930P'], ['orgasmo','https://go.hotmart.com/O50734083K?dp=1'], ['informatica', 'https://go.hotmart.com/W50745338B']]
 
 var folderTop = randomFolder()
 try {
